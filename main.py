a88516bb2e4e90249c86298a8c1a30d052de3034
Yuliya Dyachenko <y.arhangel@ya.ru>
2025-12-02 02:48:22 +0300

    change main.pu

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello from repository!')
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    # Загружаем переменные из .env
+    load_dotenv()
+
+    # Читаем переменную AUTHOR
+    author = os.getenv("AUTHOR", "Автор не указан")
+
+    print(f"Автор проекта: {author}")
+
+
+if __name__ == "__main__":
+    print("Hello from repository!")
+    print_author()
+
